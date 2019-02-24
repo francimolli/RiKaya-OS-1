ff906e96448b311a3d218aa3a1a970cf89a01ee9
Francesco Mollica <38285976+1994francis@users.noreply.github.com>
2019-02-24 18:42:24 +0100

    Add files via upload

--- pcb.c
+++ pcb.c
@@ -22,28 +22,38 @@ void freePcb(pcb_t *p){
 
 pcb_t *allocPcb(){
 	if(!list_empty(&pcbFree_h)){
-	
+
+		LIST_HEAD(pcbFree_h);
+
 		pcb_t* punt;
 		//punt è l'indirizzo dell'elemento puntato dalla sentinella estratto da list_head.
-		punt = container_of(pcbFree_h.next, pcb_t, p_next);
+		punt = container_of(pcbFree_h.prev, pcb_t, p_next);
+		//rimuovo l'indirizzo dalla lista pcbFreeh che lo contiene
+		list_del(&(punt->p_next));
+
+		INIT_LIST_HEAD(&(punt->p_next));
 
 		punt->p_parent = NULL;
 		INIT_LIST_HEAD(&(punt->p_child));
 		INIT_LIST_HEAD(&(punt->p_sib));
-		INIT_LIST_HEAD(&(punt->p_next));
+
+
 		//inizializzazione campi che costituiscono state_t p_s
 		punt->p_s.entry_hi = 0;
 		punt->p_s.cause = 0;
 		punt->p_s.status = 0;
 		punt->p_s.pc_epc = 0;
 
-		for(int i = 0; i < STATE_GPR_LEN; i++){
+		for(int i = 0; i < 29; i++){
 			punt->p_s.gpr[i] = 0;
 		}
 		punt->p_s.hi = 0;
 		punt->p_s.lo = 0;
 
-		list_del(pcbFree_h.next);
+		punt->p_semkey = NULL;
+		punt->priority = 0;
+
+
 
 		return punt;
 	}
@@ -82,7 +92,7 @@ void insertProcQ(struct list_head* head, pcb_t* p){
 
 pcb_t* headProcQ(struct list_head* head){
 	if (!list_empty(head)){
-		return container_of(head->next,pcb_t,p_next);
+		return (container_of(head->next,pcb_t,p_next));
 	}
 
 	return NULL;
