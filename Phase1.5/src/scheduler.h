ce3dd792a95aa97542f35e99b50d6257c36d412c
Francesco Mollica <38285976+1994francis@users.noreply.github.com>
2019-04-09 12:38:57 +0200

    Update scheduler.h

--- Phase1.5/src/scheduler.h
+++ Phase1.5/src/scheduler.h
@@ -4,7 +4,8 @@
 #include "types_rikaya.h"
 pcb_t* curr_proc;
 void scheduler();
-	//dichiariamo qui la ready_queue in modo che sia visibile dallo scheduler ma possa venir 		inizializzata dal main
+	//dichiariamo qui la ready_queue in modo che sia visibile dallo scheduler ma possa venir 
+        // or che non è più HIDDEN :) inizializzata NEL main.
 struct list_head ready_queue_h;
 void kill_proc();
 pcb_t* rec_proc_kill(pcb_t* tmp);
