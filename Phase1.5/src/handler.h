705a0f90e38c244806d24535616285e065536fe6
cotus997 <45920961+cotus997@users.noreply.github.com>
2019-04-23 19:38:49 +0200

    Add files via upload

--- Phase1.5/src/handler.h
+++ Phase1.5/src/handler.h
@@ -3,7 +3,7 @@
 
 #define CAUSE_EXCCODE(cause) (((cause) & 0x0000007C) >> 2)
 //#define CAUSE_INTERRUPT(cause) (((cause) & CAUSE_IP_MASK) >> 8)
-#define CAUSE_IP_GET(cause, int_no) ((cause) & (1 << ((int_no) + 8))) //And tra cause e 1, shiftato di n posizioni
+#define CAUSE_IP_GET(cause, int_no) ((cause) & (1 << ((int_no) + 8))) //Faccio un and tra vero e il registro cause e shifto di n posizioni
 
 #include <umps/libumps.h>
 #include "const.h"
